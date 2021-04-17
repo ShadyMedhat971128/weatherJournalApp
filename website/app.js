20d2bc1ef0e6a20f7c917477b6a2bc16a6386070
ShadyMedhat971128 <45040309+ShadyMedhat971128@users.noreply.github.com>
2021-04-17 13:52:27 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -6,13 +6,13 @@ let feelings;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
-let apiKey='&APPID=e20a528a2dd451fcdd5a6417d636de6e';
+const apiKey='&APPID=e20a528a2dd451fcdd5a6417d636de6e&units=metric';
 //baseURL
 //ex. http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e20a528a2dd451fcdd5a6417d636de6e
-let baseURL='http://api.openweathermap.org/data/2.5/weather?q=';
+const baseURL='http://api.openweathermap.org/data/2.5/weather?q=';
 // Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click',performAction);
 /* Function called by event listener */
@@ -20,8 +20,11 @@ function performAction(e){
     //console.log("event listener is running")
     const zipCode = document.getElementById('zip').value;
     feelings = document.getElementById("feelings").value;
-    data =getWeatherData(baseURL,zipCode,apiKey)
-    .then(function(data){
+    tempData =getWeatherData(baseURL,zipCode,apiKey)
+    .then(function(tempData){
+        data['date']=newDate;
+        data['temp']=tempData.main.temp;
+        data['feel']=feelings;
         postData('/add',data);
     }).then(updateUI())
 }
@@ -30,7 +33,8 @@ const getWeatherData = async(url,code,key)=>{
     const response = await fetch(url+code+key);
     try{
         const newData = await response.json();
-        console.log(newData);
+        //console.log('getWeatherApp');
+        //console.log(newData);
         return newData
     } catch(error){
         console.log("error",error);
@@ -53,16 +57,33 @@ const postData = async (url='',data={})=>{
         }
 }
 /* Function to GET Project Data */
-
-
+const getData = async (url='',data={})=>{
+    const response = await fetch ( url , {
+    method : 'GET',// * GET , POST , PUT , DELETE,....
+    credentials : 'same-origin',//include * same-origin, omit,.....
+    headers : {'Content-Type' : 'application/json',},
+    body : JSON.stringify(data) ,// body data type must match "content-Type" header
+    });
+    try
+    {   const newData= await response.json();
+        //console.log(newData);
+        return newData
+    } catch (error) {
+        console.log('error',error);
+        }
+}
 /* Function to update the UI */
 const updateUI = async()=>{
     const request = await fetch('/all');
     try{
-        const alldata = await request.json();
-        document.getElementById('date').innerHTML = newDate;
-        document.getElementById('temp').innerHTML = alldata.main.temp;
-        document.getElementById('content').innerHTML = feelings;
+        const allData = await request.json()
+        .then(function (allData){
+            console.log('allData at client side');
+            console.log(allData);
+            document.getElementById('date').innerHTML = `Date:${allData.date}`;
+            document.getElementById('temp').innerHTML = `Temperature:${allData.temp}`;
+            document.getElementById('content').innerHTML = `I feel:${allData.feel}`;
+        })
     }catch(error){
         console.log("error : ",error);
     }
